2404706defe50632cb93da223aed0444e1c003b5
MdTalha707 <mdsagor0321@gmail.com>
2023-10-16 19:09:15 +0600

    my codes for right now

---
+++ src/Pages/Main/Contact/Contact.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import AboutUsBanner from '../../../Component/Main/Home/Contact-Compo/AboutUsBanner'
+import Locatoin from '../../../Component/Main/Home/Contact-Compo/Locatoin'
+import ContactForm from '../../../Component/Main/Home/Contact-Compo/ContactForm'
+import OurTeam from '../../../Component/Main/Home/Contact-Compo/OurTeam'
+import Testimonial from '../../../Component/Main/Home/Contact-Compo/Testimonial'
+
+const Contact = () => {
+  return (
+      <>
+          <AboutUsBanner />
+          <Locatoin />
+          <ContactForm />
+          <OurTeam />
+          <Testimonial/>
+      </>
+  )
+}
+
+export default Contact
