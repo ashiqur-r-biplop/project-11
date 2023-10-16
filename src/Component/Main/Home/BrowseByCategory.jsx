a4d194ad1ffafd9cfcda3b1f6920bbf3a321c9fd
Ashiqur Rahman Biplop <biplops22@gmail.com>
2023-10-16 10:14:23 +0600

    added some some pages and added folder stucture and section title common component

---
+++ src/Component/Main/Home/BrowseByCategory.jsx
@@ -0,0 +1,14 @@
+import SectionTitle from "./SectionTitle/SectionTitle";
+
+const BrowseByCategory = () => {
+  return (
+    <div>
+      <SectionTitle
+        title="Browse by category"
+        subTitle="Find the job that’s perfect for you. about 800+ new jobs everyday"
+      ></SectionTitle>
+    </div>
+  );
+};
+
+export default BrowseByCategory;
---
+++ src/Component/Main/Home/JobsOfTheDay.jsx
@@ -0,0 +1,14 @@
+import SectionTitle from "./SectionTitle/SectionTitle";
+
+const JobsOfTheDay = () => {
+  return (
+    <div>
+      <SectionTitle
+        subTitle="Search and connect with the right candidates faster."
+        title="Jobs of the day"
+      ></SectionTitle>
+    </div>
+  );
+};
+
+export default JobsOfTheDay;
