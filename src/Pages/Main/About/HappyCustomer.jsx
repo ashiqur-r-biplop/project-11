39a4198d2f4745c76f09e45b7c3912a04346159d
Abdullah Al Akash <aabdullahalakash@gmail.com>
2023-10-16 17:24:53 +0600

    About Section Complete

---
+++ src/Pages/Main/About/HappyCustomer.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const HappyCustomer = () => {
+    return (
+        <div>
+            <div className="flex justify-center">
+                <div className='text-center mt-16 lg:w-1/2 p-4'>
+                    <h1 className='text-4xl mt-2 font-bold'>Our Happy Customer</h1>
+                    <p className='mt-4 text-xl text-neutral-500'>When it comes to choosing the right web hosting provider, we know how easy it
+                        is to get overwhelmed with the number.</p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default HappyCustomer;
