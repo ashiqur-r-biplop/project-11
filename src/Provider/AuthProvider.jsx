8dd5ca4c6af97e0ffbc6c93ceffaddded45ebde3
Ashiqur Rahman Biplop <biplops22@gmail.com>
2023-10-16 11:47:27 +0600

    added auth context

--- src/Provider/AuthProvider.jsx
+++ src/Provider/AuthProvider.jsx
@@ -1,8 +1,8 @@
 import { createContext } from "react";
 
 export const AuthContext = createContext(null);
-const AuthProvider = () => {
-  return <div></div>;
+const AuthProvider = ({ children }) => {
+  return children;
 };
 
 export default AuthProvider;
