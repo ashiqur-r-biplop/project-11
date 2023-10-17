d06fd91d5609ac12309462b1ab59bc44e89aea44
AhaD <abuahad35188@gmail.com>
2023-10-17 14:08:44 +0600

    fech data ande create card all jobs

--- src/Pages/Main/All-Jobs/AllJobs.jsx
+++ src/Pages/Main/All-Jobs/AllJobs.jsx
@@ -1,9 +1,17 @@
+import { useEffect, useState } from "react";
+
  
 
   
 
 const AllJobs = () => {
-      
+     const [jobs, setJobs] = useState([]);
+     useEffect(() => {
+       fetch("../../../../public/Jobs.json")
+         .then((res) => res.json())
+         .then((data) => setJobs(data));
+     }, []);
+     console.log(jobs);
   return (
     <section className="container md:w-[1440px] mt-20 mx-auto">
           <div className="hero md:w-[1440px] bg-[#f2f6fd] my-2">
@@ -40,13 +48,67 @@ const AllJobs = () => {
       </div>
 
        {/* dewar system */}
-       <div className="drawer lg:drawer-open">
+       <div className="drawer lg:drawer-open ">
         <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
         <div className="drawer-content flex flex-col items-center justify-center">
           {/* Page content here */}
           <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
                 
               {/* card */}
+              {
+                    jobs?.map((d)=>{
+                         return(
+                              <div
+                              key={d?.id}
+                              className="border bg-[#F8FAFF] hover:bg-transparent transition-all py-10 px-4 rounded"
+                            >
+                              <div className="flex items-start justify-between pb-5">
+                                <div className="flex items-center gap-5">
+                                  <div>
+                                    <img src={d?.company_photo} alt="" />
+                                  </div>
+                                  <div>
+                                    <h2 className="text-xl font-semibold">{d?.job_portal}</h2>
+                                    <p>location</p>
+                                  </div>
+                                </div>
+                                <span>flash icon</span>
+                              </div>
+                              <div>
+                                <h1 className="font-bold text-xl">{d?.Job_title}</h1>
+                                <div className="flex items-center gap-10 text-gray-500 py-3">
+                                  <p>{d?.job_type}</p>
+                                  <p>{d?.time} minutes ago</p>
+                                </div>
+                                <p className="py-3">{d?.description.slice(0, 100)}...</p>
+                              </div>
+                              <div className="flex items-center gap-5 my-5">
+                                {d.requerment.map((r, i) => {
+                                  return (
+                                    <div key={i}>
+                                      <button className="bg-gray-400 px-2 rounded-lg w-f">
+                                        {r}
+                                      </button>
+                                    </div>
+                                  );
+                                })}
+                              </div>
+                              <div className="flex justify-between items-center py-3">
+                                <h1>
+                                  <span className="text-[#3C65F6] text-3xl font-semibold">
+                                    ${d?.selary}
+                                  </span>{" "}
+                                  <span>/Hour</span>
+                                </h1>
+                                <button className="bg-[#E0E6F7] px-4 py-3 rounded-lg text-[#2b67ff]">
+                                  Apply Now
+                                </button>
+                              </div>
+                            </div>            
+                              
+                         );
+                    })
+               }
           </div>
          
 
@@ -54,7 +116,7 @@ const AllJobs = () => {
             Open drawer
           </label>
         </div>
-        <div className="drawer-side">
+        <div className="drawer-side mt-12 md:mt-0">
         <label htmlFor="my-drawer-2" aria-label="close sidebar" className="drawer-overlay"></label> 
           <form className="menu p-4 w-80 min-h-full bg-base-200 form-control">
             {/* Sidebar content here */}
