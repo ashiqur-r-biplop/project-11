e3db0d7ae1fe3b897cce927a8d764d5fe384c395
MdTalha707 <mdsagor0321@gmail.com>
2023-10-17 11:51:25 +0600

    full page is completed

--- src/Pages/Main/Contact/Contact.jsx
+++ src/Pages/Main/Contact/Contact.jsx
@@ -11,7 +11,7 @@ const Contact = () => {
           <AboutUsBanner />
           <Locatoin />
           <ContactForm />
-          <OurTeam />
+          {/* <OurTeam /> */}
           <Testimonial/>
       </>
   )
