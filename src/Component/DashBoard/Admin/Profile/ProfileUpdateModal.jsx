4501919a229ab04da3b01734a67fc97240941123
Ashiqur Rahman Biplop <biplops22@gmail.com>
2023-10-20 00:56:51 +0600

    added some codew

--- src/Component/DashBoard/Admin/Profile/ProfileUpdateModal.jsx
+++ src/Component/DashBoard/Admin/Profile/ProfileUpdateModal.jsx
@@ -1,25 +1,24 @@
 // import React from 'react';
 
 const ProfileUpdateModal = () => {
-    return (
-        <div>
-            {/* The button to open modal */}
-           
-            {/* Put this part before </body> tag */}
-            <input type="checkbox" id="my_modal_7" className="modal-toggle" />
-            <div className="modal">
-                <div className="modal-box">
-                    <h3 className="text-2xl text-blue-500 text-center font-bold">Update Profile!</h3>
+  return (
+    <div>
+      {/* The button to open modal */}
 
-
-
-
-                    
-                </div>
-                <label className="modal-backdrop" htmlFor="my_modal_7">Close</label>
-            </div>
+      {/* Put this part before </body> tag */}
+      <input type="checkbox" id="my_modal_7" className="modal-toggle" />
+      <div className="modal">
+        <div className="modal-box">
+          <h3 className="text-2xl text-blue-500 text-center font-bold">
+            Update Profile!
+          </h3>
         </div>
-    );
+        <label className="modal-backdrop" htmlFor="my_modal_7">
+          Close
+        </label>
+      </div>
+    </div>
+  );
 };
 
 export default ProfileUpdateModal;
